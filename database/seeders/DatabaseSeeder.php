faa83c0cf3a00189de706aa889bb911673143803
Reirafan <thealanjack@gmail.com>
2026-06-22 17:20:07 +0700

    update akun

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,5 +22,14 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        User::updateOrCreate(
+            ['email' => 'admin@travel.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('admin12345'),
+                'is_admin' => true,
+            ]
+        );
     }
 }
