12051a11d514ddc0ba57a1a751701f8093bfb727
reijack <thealanjack@gmail.com>
2026-09-01 11:30:53 +0700

    add Render deployment config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     $middleware->alias([
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ]);
+    // Render sits behind a reverse proxy/load balancer, so trust it for
+    // correct scheme (https) and client IP detection.
+    $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
